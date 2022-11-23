7cb1c9a3f67ef47a88b50fc5adda5f0f55fb637b
atsushi-maru <ikeda.atsushi00@gmail.com>
2022-11-23 18:45:10 +0900

    create animation.html, .js, .css

--- js/animation.js
+++ js/animation.js
@@ -16,4 +16,37 @@ $(function() {
     // フェードイン or フェードアウトする
     $('.box2').fadeToggle();
   })
+
+  // id属性がslideUpの要素がクリックされたら
+  $('#slideUp').on('click', function() {
+    // スライドアップする
+    $('.box3').slideUp();
+  });
+  // id属性がslideUpの要素がクリックされたら
+  $('#slideDown').on('click', function() {
+    // スライドダウンする
+    $('.box3').slideDown();
+  });
+  // id属性がslideUpの要素がクリックされたら
+  $('#slideToggle').on('click', function() {
+    // スライドアップ or スライドダウンする
+    $('.box4').slideToggle();
+  });
+
+  $('#fadeOut2').on('click', function() {
+    // 不透明度を0にする(フェードアウトする)
+    $('.box5').css('opacity', 0);
+  });
+  $('#fadeIn2').on('click', function() {
+    // 不透明度を1にする(フェードインする)
+    $('.box5').css('opacity', 1);
+  });
+  $('#slideUp2').on('click', function() {
+    // 高さを0にする(スライドアップする)
+    $('.box5').css('height', 0);
+  });
+  $('#slideDown2').on('click', function() {
+    // 高さを200にする(スライドダウンする)
+    $('.box5').css('height', 200);
+  });
 });
